5e094b22af6fe10d63ffd74539ebcef1bc018baf
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-02-06 02:27:27 +0300

    all done

--- app/components/Github.jsx
+++ app/components/Github.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { Component } from "react";
 import { FaGithub } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
--- app/head.jsx
+++ app/head.jsx
@@ -3,8 +3,17 @@ export default function Head() {
     <>
       <title>Didar Abdulkhaliq</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Didar Abdulkhaliq's personal website" />
+      <meta name="description" content="Didar Abdulkhaliq's personal portfolio website" />
       <link rel="icon" href="/favicon.ico" />
+      <meta property="og:image" content="https://i.imgur.com/52NnUgf.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:title" content="Didar Abdulkhaliq" />
+      <meta property="og:description" content="Didar Abdulkhaliq's personal portfolio website" />
+      <meta property="og:url" content="https://www.didar.dev" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Didar Abdulkhaliq" />
+      <meta property="og:locale" content="en_US" />
     </>
   )
 }
