c570f7560c690e53d51cef243de1197debc3e390
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-02-07 10:25:26 +0300

    components folder moved

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
 "use client";
-import Navbar from './components/Navbar'
-import Header from "./components/Header"
-import About from "./components/About"
-import Skills from "./components/Skills"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
-import Github from "./components/Github"
+import Navbar from '../components/Navbar'
+import Header from "../components/Header"
+import About from "../components/About"
+import Skills from "../components/Skills"
+import Projects from "../components/Projects"
+import Contact from "../components/Contact"
+import Github from "../components/Github"
 
 export default function Home() {
   return (
