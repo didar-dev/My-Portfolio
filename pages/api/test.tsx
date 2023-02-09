34ba28891d6abbe1c873482961fc26b0d5074e33
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-02-10 00:20:07 +0300

    api test

--- pages/api/test.tsx
+++ pages/api/test.tsx
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
-  return {
-    status: 200,
-    body: "Hello world!",
-  };
+  // req is Node.js http.IncomingMessage, plus some pre-built middlewares
+  // res is Node.js http.ServerResponse, plus some helper functions
+  // More info: https://nextjs.org/docs/api-routes/introduction
+
+  res.status(200).json({ text: "Hello" });
 }
