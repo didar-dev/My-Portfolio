bb25e8606a0c085b13702166cb5873df678f3bfe
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-02-12 00:10:05 +0300

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,20 +2,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-3FW29S7XTM"
-        ></Script>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-3FW29S7XTM');
-`}
-        </Script>
-      </head>
+      <head></head>
       <body>{children}</body>
     </html>
   );
