72de9561805424f2ec1c11d5315db4f0a3eaaf31
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-02-12 00:07:31 +0300

    google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,20 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3FW29S7XTM"
+        ></Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3FW29S7XTM');
+`}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
