04e658d7f29ccc1ccd72497240165ed79c37748d
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-02-23 00:54:27 +0300

    Content-Security-Policy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  async headers() {
+    return [
+      {
+        // Apply a Content-Security-Policy header to all pages
+        source: "/",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self' data:; connect-src 'self';",
+          },
+        ],
+      },
+    ];
   },
 };
 
