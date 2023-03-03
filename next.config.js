f856e2607ee13d4a952b3fe3cd0bf9ea5a6dc427
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-03-03 22:39:07 +0300

    cors

--- next.config.js
+++ next.config.js
@@ -15,15 +15,16 @@ const nextConfig = {
               "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self' data:; connect-src 'self';",
           },
           { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*", // Proxy to Backend
       },
     ];
   },
