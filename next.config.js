e6029ad53cfa9c7da4151f77cca47c53cdfd2996
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-03-03 22:41:23 +0300

    eeeeee

--- next.config.js
+++ next.config.js
@@ -4,30 +4,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self' data:; connect-src 'self';",
-          },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
